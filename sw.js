5ae0edfecc3ca0ae7f94c68ddbd9eecc0c0c956f
shinV <38inch3@gmail.com>
2025-09-10 17:42:09 +0900

    sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,34 @@
 const CACHE_NAME = "shinbawi-cache-v1";
 
-// 설치 단계: 기본 파일 캐싱
-self.addEventListener("install", e => {
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
-    })
-  );
-});
-
-// 활성화 단계: 오래된 캐시 제거
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => {
-        if (k !== CACHE_NAME) {
-          return caches.delete(k);
-        }
-      }))
+    caches.open(CACHE_NAME).then((cache) =>
+      cache.addAll([
+        "/shinbawiproblems/",
+        "/shinbawiproblems/index.html",
+        "/shinbawiproblems/manifest.json"
+      ])
     )
   );
 });
 
-// 요청 가로채기: 네트워크 우선, 실패 시 캐시
-self.addEventListener("fetch", e => {
+self.addEventListener("activate", (e) => {
+  clients.claim();
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
   e.respondWith(
     fetch(e.request)
-      .then(response => {
-        return caches.open(CACHE_NAME).then(cache => {
-          cache.put(e.request, response.clone());
-          return response;
-        });
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then((c) => c.put(e.request, copy));
+        return res;
       })
       .catch(() => caches.match(e.request))
   );
